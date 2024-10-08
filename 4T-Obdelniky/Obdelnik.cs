d830e32e7d42df053b4bf8d62cd1b652de4033db
Ondřej Šesták <Ondrej.Sestak@student.soskolin.cz>
2024-10-08 11:03:13 +0200

    Čtverec

--- 4T-Obdelniky/Obdelnik.cs
+++ 4T-Obdelniky/Obdelnik.cs
@@ -6,30 +6,28 @@ using System.Threading.Tasks;
 
 namespace _4T_Obdelniky
 {
-    public class Obdelnik
+    public class Obdelnik : Čtverec
     {
-        public int StranaA { get; set; } = 0;
         public int StranaB { get; set; } = 0;
-        public int Obsah { get => StranaA * StranaB; }
-        public int Obvod { get { return 2 * (StranaA + StranaB); } }
+        public int ObsahObdelnik { get => StranaA * StranaB; }
+        public int ObvodObdelnik { get { return 2 * (StranaA + StranaB); } }
 
 
-        public Obdelnik(int stranaA, int stranaB)
+        public Obdelnik(int stranaA, int stranaB) : base(stranaA)
         {
-            StranaA = stranaA;
-            StranaB = stranaB;
+            this.StranaB = stranaB;
         }
-        public Obdelnik(string CSVLine)
+
+        public Obdelnik(string stranaA, string stranaB) : base(stranaA)
+        {
+            this.StranaB = Convert.ToInt32(stranaB);
+        }
+        public Obdelnik(string CSVLine) : base(CSVLine)
         {
             StranaA = Convert.ToInt32(CSVLine.Split(';')[0]);
             StranaB = Convert.ToInt32(CSVLine.Split(';')[1]);
         }
-        public Obdelnik(string stranaA, string stranaB)
-        {
-            StranaA = Convert.ToInt32(stranaA);
-            StranaB = Convert.ToInt32(stranaB);
-        }
-        public virtual string ToCSV() => StranaA + ";" + StranaB;
+        public override string ToCSV() => base.ToCSV() +";"+ StranaB;
 
         public override string ToString() => "Obdelník " + " strana A: " + StranaA + ", strana B: " + StranaB;
         //$"Obdélník strana A: {stranaA}, stranaB: {stranaB}";
@@ -47,11 +45,37 @@ namespace _4T_Obdelniky
 
         }
 
-        public int Obvod => base.Obvod/2+StranaC;
+        public int ObvodTrojuhelnik => base.Obvod/2+StranaC;
+
+
 
         public override string ToCSV()
         {
             return base.ToCSV() + ";" + this.StranaC;
         }
+    }
+    public class Čtverec
+    {
+        public int StranaA { get; set; } = 0;
+        public int Obsah { get => StranaA * StranaA; }
+        public int Obvod { get { return 4 * StranaA; } }
+
+        public Čtverec(int stranaA)
+        {
+            StranaA = stranaA;
+        }
+
+        public Čtverec(string stranaA)
+        {
+            StranaA = Convert.ToInt32(stranaA);
+        }
+
+        public Čtverec(string CSVline, string prazdny)
+        {
+            StranaA = Convert.ToInt32(CSVline.Split(';'));
+        }
+
+        public virtual string ToCSV() => StranaA.ToString();
+        public override string ToString() => "Čtverec " + " strana A: " + StranaA;
     }
 }
